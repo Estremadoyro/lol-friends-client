3d02b66a1bbaa5da542a5aa676023c7b92990d02
Estremadoyro <lmestremadoyro@gmail.com>
2021-04-15 17:59:49 -0500

    production logic changed, migrating to develop

--- src/api/apiEnvironment.js
+++ src/api/apiEnvironment.js
@@ -1,7 +1,6 @@
 export const apiEnvironment = () => {
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV !== "production") {
     return "http://localhost:5000";
-  } else if (process.env.NODE_ENV === "production") {
-    return "https://lol-friends-server.herokuapp.com";
   }
+  return "https://lol-friends-server.herokuapp.com";
 };
